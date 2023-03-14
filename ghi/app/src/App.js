143f816d7d6c96b6857cdb08fab5d2e04408964d
Jamie L <jamie60633@gmail.com>
2023-03-14 12:05:30 -0700

    app.js & nav.js

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './MainPage';
+import Nav from './Nav';
+import LoginForm from './users/LoginForm';
+import Logout from './users/Logout';
+import SignupForm from './users/SignupForm';
+import RentalList from './rentals/RentalList';
+import RentalDetail from './rentals/RentalDetail';
+import RentalCreateForm from './rentals/RentalCreateForm';
+import BookingCreateForm from './bookings/BookingCreateForm';
+import BookingDetail from './bookings/BookingDetail';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <div style={{ height: 56 }}></div>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/users">
+            {/* <Route path="" element={<UserList />} /> */}
+            <Route path="login" element={<LoginForm />} />
+            <Route path="logout" element={<Logout />} />
+            <Route path="signup" element={<SignupForm />} />
+            {/* <Route path=":userId" element={<UserDetail />} /> */}
+            {/* <Route path=":userId/edit" element={<userEditForm />} /> */}
+          </Route>
+          <Route path="/rentals">
+            <Route path="" element={<RentalList />} />
+            <Route path="new" element={<RentalCreateForm />} />
+            <Route path=":rentalId" element={<RentalDetail />} />
+            {/* <Route path=":userId/rentals" element={<RentalListByHost />} /> */}
+          </Route>
+          <Route path="/bookings">
+            {/* <Route path=":userId/bookings" element={<BookingListByHost />} /> */}
+            <Route path="new" element={<BookingCreateForm />} />
+            <Route path=":bookingId" element={<BookingDetail />} />
+            {/* <Route path=":bookingId/edit" element={<BookingEditForm />} /> */}
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
